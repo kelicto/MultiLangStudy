e33aad4142a7230e4b09e261288967265aa9822e
KeLi <kelistudy@163.com>
2019-11-22 01:08:05 +0800

    Add project files.

---
+++ KeLi.MultiLangStudy.App/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using KeLi.MultiLangStudy.App.Properties;
+
+namespace KeLi.MultiLangStudy.App
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Default Lang: ");
+            Console.WriteLine(Resources.LANG_TEST);
+            Console.WriteLine();
+
+            Console.Write("zh-CN Lang: ");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("zh-CN");
+            Console.WriteLine(Resources.LANG_TEST);
+            Console.WriteLine();
+
+            Console.Write("zh-TW Lang: ");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("zh-TW");
+            Console.WriteLine(Resources.LANG_TEST);
+            Console.WriteLine();
+
+            Console.Write("en-US Lang: ");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+            Console.WriteLine(Resources.LANG_TEST);
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
